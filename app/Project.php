e2933e24387e897fa82b29fa4876c08e1129ce58
Bishal Sahoo <mavericksthinker@gmail.com>
2019-06-20 10:15:48 +0700

    modified project model

--- app/Project.php
+++ app/Project.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Project extends Model
 {
@@ -66,8 +67,8 @@ class Project extends Model
     {
         if($this->wasChanged())
             return [
-                'before' => array_except(array_diff($this->old, $this->getAttributes()),'updated_at'),
-                'after' => array_except($this->getChanges(),'updated_at')
+                'before' => Arr::except(array_diff($this->old, $this->getAttributes()),'updated_at'),
+                'after' => Arr::except($this->getChanges(),'updated_at')
             ];
     }
 
